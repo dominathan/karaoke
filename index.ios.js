5476f66ce9a6739b213417c0e0ae5175c2b75052
Louise Wilson <louisebrookswilson@gmail.com>
2016-06-14 14:30:55 -0400

    camera pull

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,22 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 class karaoke extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <TextInput style={styles.text}>
+
+        </TextInput>
       </View>
     );
   }
